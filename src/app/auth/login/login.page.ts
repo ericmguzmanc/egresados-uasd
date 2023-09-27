eb9020fe5598fb5ad7f7b011a394cd6f58d15a59
elizonRL <elizorodriguez16@gmail.com>
2023-09-27 11:08:52 -0400

    login funcionalidad

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { LoginService } from '../../service/auth/login.service';
+import { Router } from '@angular/router';
+import { LoginRequest } from 'src/app/service/auth/loginRequest';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,27 +17,54 @@ import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms'
 export class LoginPage implements OnInit {
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password:  new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor(
+    private loadingCtrl: LoadingController,
+    private loginService: LoginService,
+    private Ruter : Router
+  ) {}
+
+  ngOnInit() {}
+
   get email() {
     return this.loginForm.controls.email;
   }
+
   get password() {
     return this.loginForm.controls.password;
   }
-  login(){
-    console.log(this.loginForm.status);
-    if(!this.loginForm.valid){
-      this.loginForm.markAllAsTouched();
-      console.log("Error");
-    }
-  this.loginForm.reset();
-  
-}
 
+  login() {
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (err) => {
+          console.error(err)
+        },
+        complete: () => {
+          console.info('complete')
+          //this.loadingCtrl.dismiss();
+        }
+      });
+      this.showLoading();
+      this.Ruter.navigateByUrl('/');
+      this.loginForm.reset();
+    } else {
+      this.loginForm.markAllAsTouched();
+      console.log('Error');
+    }
+  }
+
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Dismissing after 3 seconds...',
+      duration:3000
+    });
+
+    loading.present();
+  }
 }
---
+++ src/app/service/auth/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { LoginRequest } from './loginRequest';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials:LoginRequest):Observable<any> {
+    return this.http.get('http://localhost:3006/usuario');
+  }  
+}
