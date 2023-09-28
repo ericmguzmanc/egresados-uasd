932617ae4c188d9f75c21c57fbe46e9a140a5e21
Eric Guzman <ericmguzmanc@gmail.com>
2023-09-28 08:34:05 -0400

    Login page bootstrap

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/shared/services/login.service';
+import { LoginRequest } from 'src/app/shared/interfaces/loginRequest.interface';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,27 +13,59 @@ import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms'
 export class LoginPage implements OnInit {
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password:  new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor(
+    private location: Location,
+    private loginService: LoginService,
+    private router: Router,
+    private loadingController: LoadingController
+  ) {}
+
+  ngOnInit() {}
+
   get email() {
     return this.loginForm.controls.email;
   }
+
   get password() {
     return this.loginForm.controls.password;
   }
-  login(){
-    console.log(this.loginForm.status);
-    if(!this.loginForm.valid){
-      this.loginForm.markAllAsTouched();
-      console.log("Error");
-    }
-  this.loginForm.reset();
-  
-}
 
+  login() {
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+        complete: () => {
+          console.info('complete');
+          //this.loadingCtrl.dismiss();
+        },
+      });
+      this.showLoading();
+      this.router.navigateByUrl('/');
+      this.loginForm.reset();
+    } else {
+      this.loginForm.markAllAsTouched();
+      console.log('Error');
+    }
+  }
+
+  async showLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Dismissing after 3 seconds...',
+      duration: 3000,
+    });
+
+    loading.present();
+  }
+
+  onBackButtonClick(): void {
+    this.location.back();
+  }
 }
